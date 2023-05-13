de42e3b2ae6ee9a53e9d8823a0cc2babcdfc5e82
Thomas Michelet <thomas.michelet@wifirst.fr>
2023-05-13 09:55:28 +0200

    add vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import classNames from "classnames"
+import { Analytics } from '@vercel/analytics/react';
+
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
 
@@ -14,18 +16,21 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={classNames(inter.className)}>
-        <Header />
-        <div className="container mx-auto">
-          <div className="flex justify-center bg-neutral-500 min-h-screen px-4">
-            <div className="max-w-4xl mt-8 ">
-              {children}
+    <>
+      <html lang="en">
+        <body className={classNames(inter.className)}>
+          <Header />
+          <div className="container mx-auto">
+            <div className="flex justify-center bg-neutral-500 min-h-screen px-4">
+              <div className="max-w-4xl mt-8 ">
+                {children}
+              </div>
             </div>
           </div>
-        </div>
-        <Footer/>
-      </body>
-    </html>
+          <Footer/>
+        </body>
+      </html>
+      <Analytics/>
+    </>
   )
 }
