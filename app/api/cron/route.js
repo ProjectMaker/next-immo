c993f52d601360d8e1f69fa6a6d03a9c3ddd5305
Thomas Michelet <thomas.michelet@wifirst.fr>
2024-05-07 14:03:21 +0200

    fix bearer

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -1,12 +1,11 @@
 import { NextResponse } from 'next/server';
 import sync from './sync-ftp-to-db'
 export async function GET(req) {
-  const token = req.headers.get('authorization')
   const authorized = process.env.VERCEL_ENV === "development"
-    || process.env.CRON_SECRET === `Bearer ${token}`
+    || req.headers.get('authorization') === `Bearer ${process.env.CRON_SECRET}`
   if (authorized) {
     await sync()
     return NextResponse.json({ status: 200 });
   }
-  return new NextResponse(`Bad token ${token}`, { status: 401 })
+  return new NextResponse(`Unauthorized`, { status: 401 })
 }
