8e7f533038340d49390eb4d48ee2f7f7420d32a7
Thomas Michelet <thomas.michelet@wifirst.fr>
2024-05-07 10:43:30 +0200

    return bad token

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -1,12 +1,12 @@
 import { NextResponse } from 'next/server';
 import sync from './sync-ftp-to-db'
 export async function GET(req) {
+  const token = req.headers.get('authorization')
   const authorized = process.env.VERCEL_ENV === "development"
-    ? true
-    : req.headers.get('Authorization') === `Bearer ${process.env.CRON_SECRET}`
+    || req.headers.get('authorization') === `Bearer ${token}`
   if (authorized) {
     await sync()
     return NextResponse.json({ status: 200 });
   }
-  return NextResponse.json({ status: 401 });
+  return NextResponse.json({error: `Bad token ${token}`}, { status: 401 });
 }
